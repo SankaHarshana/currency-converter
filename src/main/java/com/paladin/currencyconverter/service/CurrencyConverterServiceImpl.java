df8b1586eb12adb61563c8e155cba36d7cc0863f
Sanka Harshana <sankaharshana@gmail.com>
2021-06-18 23:16:01 +0530

    yml file update

--- src/main/java/com/paladin/currencyconverter/service/CurrencyConverterServiceImpl.java
+++ src/main/java/com/paladin/currencyconverter/service/CurrencyConverterServiceImpl.java
@@ -26,36 +26,38 @@ public class CurrencyConverterServiceImpl implements CurrencyConverterService {
         if(currencyConvertDto != null) {
             HashMap<String, Double> rates = new HashMap<>();
             CurrencyRatesDto res = apiCallService.apiCaller();
-            rates = res.getRates();
+            if(rates != null) {
+                rates = res.getRates();
 
-            String fromCurrency = "";
-            if (currencyConvertDto.getFromCurrency() != null
-                    && !currencyConvertDto.getFromCurrency().equals("")
-                    && rates.containsKey(currencyConvertDto.getFromCurrency())) {
-                fromCurrency = currencyConvertDto.getFromCurrency();
-            }else throw new CurrencyConverterException("Provide value for fromcurrency field doesn't support");
+                String fromCurrency = "";
+                if (currencyConvertDto.getFromCurrency() != null
+                        && !currencyConvertDto.getFromCurrency().equals("")
+                        && rates.containsKey(currencyConvertDto.getFromCurrency())) {
+                    fromCurrency = currencyConvertDto.getFromCurrency();
+                } else throw new CurrencyConverterException("Provide value for fromcurrency field doesn't support");
 
-            String toCurrency = "";
-            if (currencyConvertDto.getToCurrency() != null
-                    && !currencyConvertDto.getToCurrency().equals("")
-                    && rates.containsKey(currencyConvertDto.getToCurrency())) {
-                toCurrency = currencyConvertDto.getToCurrency();
-            }else throw new CurrencyConverterException("Provide value for tocurrency field doesn't support");
+                String toCurrency = "";
+                if (currencyConvertDto.getToCurrency() != null
+                        && !currencyConvertDto.getToCurrency().equals("")
+                        && rates.containsKey(currencyConvertDto.getToCurrency())) {
+                    toCurrency = currencyConvertDto.getToCurrency();
+                } else throw new CurrencyConverterException("Provide value for tocurrency field doesn't support");
 
-            Double eurFromCurrency = rates.get(fromCurrency);
-            Double eurToCurrency = rates.get(toCurrency);
-            Double fromToCurrency = eurFromCurrency / eurToCurrency;
+                Double eurFromCurrency = rates.get(fromCurrency);
+                Double eurToCurrency = rates.get(toCurrency);
+                Double fromToCurrency = eurFromCurrency / eurToCurrency;
 
-            Double finalValue = 0.0;
-            Double amount = 0.0;
-            if(currencyConvertDto.getAmount() != null && !currencyConvertDto.getAmount().equals(0)){
-                amount = currencyConvertDto.getAmount();
-            }else throw new CurrencyConverterException("Provide value for amount field doesn't support");
+                Double finalValue = 0.0;
+                Double amount = 0.0;
+                if (currencyConvertDto.getAmount() != null && !currencyConvertDto.getAmount().equals(0)) {
+                    amount = currencyConvertDto.getAmount();
+                } else throw new CurrencyConverterException("Provide value for amount field doesn't support");
 
-            finalValue = amount / fromToCurrency;
-            returnDto.setAmount(finalValue);
-            returnDto.setCurrency(toCurrency);
-            return returnDto;
+                finalValue = amount / fromToCurrency;
+                returnDto.setAmount(finalValue);
+                returnDto.setCurrency(toCurrency);
+                return returnDto;
+            }else throw new CurrencyConverterException("Something went wrong with the remote server");
         }else throw new CurrencyConverterException("Request body cannot be empty");
     }
 
